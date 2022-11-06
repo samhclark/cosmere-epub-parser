b9cfc4e36eccf9bc887bdac7a4c19e775e507fe9
Sam Clark <1059176+samhclark@users.noreply.github.com>
2022-11-05 22:45:19 -0500

    Open and print title

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use epub::doc::EpubDoc;
+
 fn main() {
-    println!("Hello, world!");
+    let doc = EpubDoc::new("/home/sam/Downloads/cosmere-ebooks/the-bands-of-mourning.epub");
+    assert!(doc.is_ok());
+    let doc = doc.unwrap();
+    println!("{}", doc.mdata("title").unwrap());
 }
