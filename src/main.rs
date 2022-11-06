a4a86c78f235ffbbe17d4899b1108fb16d996e69
Sam Clark <1059176+samhclark@users.noreply.github.com>
2022-11-06 15:55:50 -0600

    Closer. Prints text, preserves Unicode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use epub::doc::EpubDoc;
+use html2text::from_read;
 
 fn main() {
     let doc = EpubDoc::new("/home/sam/Downloads/cosmere-ebooks/the-bands-of-mourning.epub");
@@ -12,4 +13,9 @@ fn main() {
     doc.set_current_page(9).unwrap();
     println!("?? {:?}", doc.get_current_str());
     // dammit, it doesn't actually get me much. Just prints out the html. Could have done this after unzipping the epub
+
+    let this_page = doc.get_current().unwrap();
+    let foo = from_read(&this_page[..], 80);
+    println!("page as string?: {}", foo)
+    // okay! That kinda works. Each paragraph is separated by a blank newline. Would need to recombine the lines that from_read breaks
 }
