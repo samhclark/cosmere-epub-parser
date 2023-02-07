418506f0053547c2ba13902819b7d172441a7561
Sam Clark <1059176+samhclark@users.noreply.github.com>
2023-02-06 22:04:36 -0600

    Update styles deps, disable tracing

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ pub struct AppState {
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
-        .with_max_level(Level::TRACE)
+        .with_max_level(Level::INFO)
         .init();
 
     let tantivy_wrapper = TantivyWrapper::new();
